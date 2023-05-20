d847398e2652a50bd604d4e7cc4ea09b790cc431
落花似水却无年 <115523363+bawvf@users.noreply.github.com>
2023-05-20 22:53:57 +0800

    Add files via upload

---
+++ Big/004-Tree/001-parent/main.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "io.h"
+#include "math.h"
+#include "time.h"
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 100 /* 存储空间初始分配量 */
+#define MAX_TREE_SIZE 100 /* 二叉树的最大结点数 */
+
+typedef int Status;		/* Status是函数的类型,其值是函数结果状态代码，如OK等 */
+typedef int TElemType;  /* 树结点的数据类型，目前暂定为整型 */
+typedef TElemType SqBiTree[MAX_TREE_SIZE]; /* 0号单元存储根结点  */
+
+//结构定义
+typedef struct PTNode {
+    TElemType data;
+    int parent;
+} PTNode;
+
+typedef struct {
+    PTNode nodes[MAX_TREE_SIZE];
+    int r, n;
+} PTree;
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
